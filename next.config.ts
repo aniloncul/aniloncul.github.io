19c017ac846afde3aa69abf7c1414382ee30b264
aniloncul <aniloncul@gmail.com>
2025-12-26 13:23:34 +0100

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
       },
     ],
   },
